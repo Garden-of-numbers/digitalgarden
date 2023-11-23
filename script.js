43e998662972e69f3c4afa53d885d10a471b1c60
Miłosz <138797981+Garden-of-numbers@users.noreply.github.com>
2023-11-23 01:52:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const navButton = document.querySelector("#hamburger");
+const navLinks = document.querySelectorAll(".nawigacja");
+
+navButton.addEventListener("click", () => {
+  navLinks.forEach(navLink => {
+    navLink.classList.toggle("show");
+  });
+});
+
+const sunIconButton = document.querySelector("#sunBtn");
+const moonIconButton = document.querySelector("#moonBtn");
+const headerBackground = document.querySelector("header");
+const projectsBackground = document.querySelector("#projects");
+const newsBackground = document.querySelector("#news");
+
+sunIconButton.addEventListener("click", () => {
+  document.body.style.background = "white";
+  sunIconButton.style.display = "none";
+  moonIconButton.style.display = "block";
+  headerBackground.style.backgroundColor = "black"
+  projectsBackground.style.backgroundColor = "black"
+  newsBackground.style.backgroundColor = "black"
+});
+
+if(moonIconButton.addEventListener("click", () =>  {
+  document.body.style.background = "linear-gradient(to bottom, black, rgb(75, 0, 75))";
+  moonIconButton.style.display = "none";
+  sunIconButton.style.display = "block";
+}));
+
+
+let textArray = ["1", "2", "3"];
+
+const nextPageBtn = document.querySelector("#pageBtnNext");
+const backPageBtn = document.querySelector("#pageBtnBack");
+
+let mainPage = document.querySelector("#page");
+mainPage.innerHTML = textArray[0];
+
+nextPageBtn.addEventListener("click", () => {
+  if (mainPage.innerHTML === textArray[0]) {
+    backPageBtn.style.display = "block";
+    mainPage.innerHTML = textArray[1];
+  } else if (mainPage.innerHTML === textArray[1]) {
+    mainPage.innerHTML = textArray[2];
+  }
+});
+
+backPageBtn.addEventListener("click", () => {
+  if (mainPage.innerHTML === textArray[2]) {
+    mainPage.innerHTML = textArray[1];
+  } else if (mainPage.innerHTML === textArray[1]) {
+    mainPage.innerHTML = textArray[0];
+    backPageBtn.style.display = "none"; 
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
